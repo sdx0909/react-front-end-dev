b2af7dbded7a2635b607968e94f4706930df9b82
sdx0909 <dixitsaurabh092@gmail.com>
2025-06-18 10:32:37 +0530

    done-pagination

---
+++ pokedex/src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,13 @@
+import "./Pokemon.css";
+
+function Pokemon({ name, image }) {
+  return (
+    <div className="pokemon">
+      <div className="pokemon-name">{name}</div>
+      <div>
+        <img src={image} className="pokemon-image" />
+      </div>
+    </div>
+  );
+}
+export default Pokemon;
